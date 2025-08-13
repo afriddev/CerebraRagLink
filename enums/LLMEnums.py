378cbe76d6846bd7742e6fe76c593ea6223a755f
SHIAK AFRID <afridayan01@gmail.com>
2025-08-13 06:34:09 +0000

    Added cerebras llm service layer with proper models and enums along with status codes

---
+++ enums/LLMEnums.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class LLMChatMessageRoleEnum(Enum):
+    USER = "user"
+    SYSTEM = "system"
+
+
+class LLMChatReasoningEffortEnum(Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
+
+class LLMChatResponseStatusEnum(Enum):
+    SUCCESS = (200, "SUCCESS")
+    BAD_REQUEST = (400, "BAD_REQUEST")
+    UNAUTHROZIED = (401, "UNAUTHROZIED")
+    PERMISSION_DENIED = (403, "PERMISSION_DENIED")
+    NOT_FOUND = (404, "NOT_FOUND")
+    REQUEST_TIMEOUT = (408, "REQUEST_TIMEOUT")
+    CONFLICT = (409, "CONFLICT")
+    ENTITY_ERROR = (422, "ENTITY_ERROR")
+    RATE_LIMIT = (429, "RATE_LIMIT")
+    SERVER_ERROR = (500, "SERVER_ERROR")
---
+++ services/LLM.py
@@ -0,0 +1,65 @@
+import cerebras.cloud.sdk
+from cerebras.cloud.sdk import AsyncCerebras
+from enums.LLMEnums import LLMChatResponseStatusEnum
+from implementations.LLMImplementation import LLMImpl
+from models.LLMModels import LLMChatModel, LLMChatResponseModel
+from cerebras.cloud.sdk import DefaultAioHttpClient
+from typing import Any,cast
+
+
+#os.environ.get("CEREBRAS_API_KEY")
+
+class LLM(LLMImpl):
+    
+    def HandleApiStatusError(self,statusCode:int) -> LLMChatResponseModel:
+        errorCodes = {
+            400 : LLMChatResponseStatusEnum.BAD_REQUEST,
+            401 : LLMChatResponseStatusEnum.UNAUTHROZIED,
+            403 : LLMChatResponseStatusEnum.PERMISSION_DENIED,
+            404 : LLMChatResponseStatusEnum.NOT_FOUND
+        }
+        message = errorCodes.get(statusCode,LLMChatResponseStatusEnum.SERVER_ERROR)
+        return LLMChatResponseModel(
+                status=message
+            )
+            
+    async def Chat(self,modelParams: LLMChatModel) -> LLMChatResponseModel:
+            try:
+                async with AsyncCerebras(
+                api_key=modelParams.apiKey,
+                http_client=DefaultAioHttpClient(),
+                ) as client:
+                chat_completion = await client.chat.completions.create( # type: ignore
+                messages=cast(Any,modelParams.messages),
+                model=modelParams.model,
+                max_completion_tokens=modelParams.maxCompletionTokens,
+                stream=modelParams.stream,
+                reasoning_effort=modelParams.reasoningEffort.value,
+                temperature= modelParams.temperature
+                    )
+                return LLMChatResponseModel(
+                    status=LLMChatResponseStatusEnum.SUCCESS
+                )
+            except cerebras.cloud.sdk.APIConnectionError as e:
+                print(e)
+                return LLMChatResponseModel(
+                    status=LLMChatResponseStatusEnum.SERVER_ERROR
+                )
+                
+            except cerebras.cloud.sdk.RateLimitError as e:
+                print(e)
+                return LLMChatResponseModel(
+                    status=LLMChatResponseStatusEnum.RATE_LIMIT
+                )
+            except cerebras.cloud.sdk.APIStatusError as e:
+                print(e)
+                return self.HandleApiStatusError(e.status_code)
+                
+                
+            
+                
+            
+                
+    
+        
+        

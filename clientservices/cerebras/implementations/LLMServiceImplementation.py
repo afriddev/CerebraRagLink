baa59cc5c1141a82ebc0c1fc0055d393ffeb0fcc
afriddev <afridayan01@gmail.com>
2025-08-17 21:11:54 +0530

    feat: Update LLM and QA services to support StreamingResponse for improved real-time interaction

--- clientservices/cerebras/implementations/LLMServiceImplementation.py
+++ clientservices/cerebras/implementations/LLMServiceImplementation.py
@@ -1,14 +1,18 @@
 from abc import ABC, abstractmethod
 
+from fastapi.responses import StreamingResponse
+
 from clientservices.cerebras.models import LLMRequestModel, LLMResponseModel
 
 
 class LLMServiceImpl(ABC):
 
     @abstractmethod
-    async def Chat(self, modelParams: LLMRequestModel) -> LLMResponseModel:
+    async def Chat(self, modelParams: LLMRequestModel) -> LLMResponseModel | StreamingResponse:
         pass
 
     @abstractmethod
-    def HandleApiStatusError(self, statusCode: int) -> LLMResponseModel:
+    def HandleApiStatusError(
+        self, statusCode: int
+    ) -> LLMResponseModel :
         pass

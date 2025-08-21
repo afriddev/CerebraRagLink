bd466a7510ac401cd0493d503b20b6780109ddbb
afrid-railtelit <afridrailtelit@gmail.com>
2025-08-21 17:12:57 +0530

    Refactor code structure for improved readability and maintainability

--- k.py
+++ k.py
@@ -1,17 +1,13 @@
-
 from graphrag import FileChunkGragService
-
-
-
 a = FileChunkGragService()
 
 
 
+
 async def main():
-    await a.handleEntitiesProcess("a.pdf")
+    await a.handleEntitiesProcess("opd_manual.pdf")
     
-    
-    
+        
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main()) 

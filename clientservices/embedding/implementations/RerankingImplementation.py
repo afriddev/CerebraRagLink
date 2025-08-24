f345fc2a6c20c2c731f4a860b804322836c8530e
afriddev <afridayan01@gmail.com>
2025-08-24 17:59:40 +0530

    feat: Update reranking models and service to include response handling and improve method signatures

--- clientservices/embedding/implementations/RerankingImplementation.py
+++ clientservices/embedding/implementations/RerankingImplementation.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
-from clientservices.embedding.models import RerankingRequestModel
+from clientservices.embedding.models import (
+    RerankingRequestModel,
+    RerankingResponseModel,
+)
 
 
 class RerankingImpl(ABC):
 
     @abstractmethod
-    async def FindRankingScore(self, modelParams: RerankingRequestModel):
+    async def FindRankingScore(
+        self, modelParams: RerankingRequestModel
+    ) -> RerankingResponseModel:
         pass
